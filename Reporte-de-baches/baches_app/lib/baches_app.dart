166a5cae6e6464d6e9d68afe29d3cbdeab481ef1
Izaird Mothelet <iza_alex3@hotmail.com>
2020-10-13 15:14:14 -0500

    Modiciacion del widget BachesApp, se agrego el paquete generic_bloc_provider

--- Reporte-de-baches/baches_app/lib/baches_app.dart
+++ Reporte-de-baches/baches_app/lib/baches_app.dart
@@ -1,39 +1,71 @@
+import 'package:baches_app/User/bloc/bloc_user.dart';
+import 'package:baches_app/User/ui/screens/profile_baches.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:baches_app/Bache/ui/screens/home_bache.dart';
 import 'package:lipsum/lipsum.dart' as lipsum;
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 class BachesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home, color: Colors.indigo), title: Text("")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search, color: Colors.indigo), title: Text("")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person, color: Colors.indigo), title: Text("")),
-        ]),
+        tabBar: CupertinoTabBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.indigo,
+              ),
+              label: (""),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.indigo,
+              ),
+              label: (""),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.indigo,
+              ),
+              label: (""),
+            ),
+          ],
+        ),
         tabBuilder: (BuildContext context, int index) {
           switch (index) {
             case 0:
               return CupertinoTabView(
-                builder: (BuildContext context) =>
-                    HomeBache(lipsum.createSentence()),
+                builder: (BuildContext context) {
+                  return BlocProvider<UserBloc>(
+                    bloc: UserBloc(),
+                    child: HomeBache(lipsum.createWord()),
+                  );
+                },
               );
               break;
             case 1:
               return CupertinoTabView(
-                builder: (BuildContext context) =>
-                    HomeBache(lipsum.createSentence()),
+                builder: (BuildContext context) {
+                  return BlocProvider<UserBloc>(
+                    bloc: UserBloc(),
+                    child: ProfileBaches(),
+                  );
+                },
               );
               break;
             case 2:
               return CupertinoTabView(
-                builder: (BuildContext context) =>
-                    HomeBache(lipsum.createSentence()),
+                builder: (BuildContext context) {
+                  return BlocProvider<UserBloc>(
+                    bloc: UserBloc(),
+                    child: ProfileBaches(),
+                  );
+                },
               );
               break;
           }
