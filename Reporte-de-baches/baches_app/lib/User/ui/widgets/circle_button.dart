eb430db8b4caedb6a669a28bdb0d0b5d2560ff67
Izaird Mothelet <iza_alex3@hotmail.com>
2020-10-13 14:28:12 -0500

    Creacion del widget button bar

---
+++ Reporte-de-baches/baches_app/lib/User/ui/widgets/circle_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CircleButton extends StatefulWidget {
+  final VoidCallback onPressed;
+  final bool mini;
+  final icon;
+  final double iconSize;
+  final color;
+
+  CircleButton(this.mini, this.icon, this.iconSize, this.color, this.onPressed);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CircleButton();
+  }
+}
+
+class _CircleButton extends State<CircleButton> {
+  void onPressedButton() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: FloatingActionButton(
+      backgroundColor: widget.color,
+      mini: widget.mini,
+      onPressed: widget.onPressed,
+      child: Icon(
+        widget.icon,
+        size: widget.iconSize,
+        color: Color(0xFF4268D3),
+      ),
+    ));
+  }
+}
